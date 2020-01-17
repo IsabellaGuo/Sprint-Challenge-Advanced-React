584458cc07eb615125a79cea90dd32ed6ba13396
IsabellaGuo <isabella.guofei@gmail.com>
2020-01-17 09:00:33 -0800

    Part of App.js is done.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      data: [],
+      name: "",
+      country: "",
+    };
+  }
+
+  componentDidMount(){
+    Axios
+    .get("http://localhost:5000/api/players")
+    .then(response=>{
+      this.setState({
+        data: response.data,
+        name: response.data.name,
+        country: response.data.country
+      });
+    })
+    .catch (error => console.log("Not found"))
+  }
+  render(){
+    <div className = "App">
+      
     </div>
-  );
+  }
 }
+
 
 export default App;
